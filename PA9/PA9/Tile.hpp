01470f840c6e89df02e39397e78e6cd6c584382c
BenjaminWas <benjaminwasisco@gmail.com>
2024-12-02 11:57:44 -0800

    Started Tile class

--- PA9/PA9/Tile.hpp
+++ PA9/PA9/Tile.hpp
@@ -1,8 +1,37 @@
-/**/
+/*
+Tile Class
+friend of Lake so no getter or setters
+
+*/
+#include "Fish.hpp"
+
+class Lake;
+
 class Tile
 {
+	friend class Lake;
 public:
-
+	//
+	Tile();
+	~Tile();
+	//
+	void update(bool, bool);
 private:
-
+	Fish* fishPtr;
+	bool selected, fish;
 };
+//
+Tile::Tile()
+{
+	fishPtr = nullptr;
+	selected = false;
+	fish = false;
+}
+Tile::~Tile()
+{
+	delete(fishPtr);
+
+}
+//
+
+//
