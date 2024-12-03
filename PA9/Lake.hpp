d153d7262011ad809500aee72a792fbcd005b2da
Naven Christian Laya <43055343+navenlaya@users.noreply.github.com>
2024-12-02 18:03:51 -0800

    Update Lake.hpp

--- PA9/Lake.hpp
+++ PA9/Lake.hpp
@@ -2,11 +2,20 @@
 #pragma once
 #include "Tile.hpp"
 #define MAX_SIZE 25
+#include <SFML/Graphics.hpp>
+
 class Lake
 {
 public:
+	Lake(sf::RenderWindow* window);
+	void draw();
 
 private:
 	Tile lake[MAX_SIZE][MAX_SIZE];
-	sf::RenderWindow* window;
+	sf::RectangleShape pier;
+	sf::RectangleShape shadow;
+	sf::RenderWindow* window; 
+
+	void createTiles();
+	void createPier();
 };
