96a813248e3da3c67fdd8d4aa1072896b3dd7bdc
coletteaddah <146807778+coletteaddah@users.noreply.github.com>
2024-12-03 23:58:11 -0800

    Add files via upload

--- PA9/main.cpp
+++ PA9/main.cpp
@@ -1,10 +1,13 @@
 /**/
 #include "Lake.hpp"
+#define TILE_SIZE 32
+using namespace sf;
+
 int main(void)
 {
     sf::RenderWindow app(sf::VideoMode(MAX_SIZE * TILE_SIZE, MAX_SIZE * TILE_SIZE, 8), "Fishing Simulator", sf::Style::Close);
     /* For Debugging opens window
-	while (app.isOpen())
+    while (app.isOpen())
     {
         // Process events
         sf::Event event;
@@ -23,5 +26,42 @@ int main(void)
     }
    */
 
-return 0;
+    Texture t1, t2;
+    t1.loadFromFile("images/water.png");
+    t2.loadFromFile("images/dock.jpeg");
+
+
+    Sprite waterSprite(t1);
+    Sprite woodSprite(t2);
+
+    // Scale sprites to fit the tile 
+    waterSprite.setScale(TILE_SIZE / (float)t1.getSize().x, TILE_SIZE / (float)t1.getSize().y);
+    woodSprite.setScale(TILE_SIZE / (float)t2.getSize().x, TILE_SIZE / (float)t2.getSize().y
+    );
+
+    while (app.isOpen()) {
+        sf::Event event;
+        while (app.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                app.close();
+        }
+
+        // Clear screen
+        app.clear();
+
+        for (int i = 0; i < MAX_SIZE; i++) {
+            for (int j = MAX_SIZE / 2; j < MAX_SIZE; j++) { waterSprite.setPosition(i * TILE_SIZE, j * TILE_SIZE);
+            app.draw(waterSprite); }
+        }
+
+        // Draw  dock 
+        for (int i = 8; i < 16; i++)
+        {
+            woodSprite.setPosition(i * TILE_SIZE, (MAX_SIZE / 2 - 1) * TILE_SIZE);
+            app.draw(woodSprite);
+        }
+        app.display();
+    }
+
+    return 0;
 }
